9aee7409b7b854b798093221d14e072186bf7fd5
xb <934814993@qq.com>
2019-07-05 16:06:23 +0800

    RGB

---
+++ hsv_detector/hsv_detector.cpp
@@ -0,0 +1,132 @@
+
+#include<opencv2/opencv.hpp>
+#include <opencv2/highgui/highgui_c.h>
+#include<iostream>
+ 
+using namespace cv;
+using namespace std;
+ 
+void hsv_detector(Mat image)
+{
+    Mat hsvImg;
+
+    //将RGB颜色空间转换为HSV颜色空间
+    cvtColor(image, hsvImg, COLOR_BGR2HSV); //CV_BGR2YCrCb
+
+    imshow("hsv", hsvImg);
+
+    int minH = 0; //26
+    int maxH = 0; //34
+
+    int minS = 0; //
+    int maxS = 0;
+
+    int minV = 0; //200
+    int maxV = 0;
+
+    Mat yellowImg, blueImg, redImg, greenImg;
+
+    for (int i = 0; i < 4; i++)
+    {
+        switch (i)
+        {
+            case 0: //黄色
+                minH = 16; //26
+                maxH = 35; //34
+
+                minS = 160; //
+                maxS = 255;
+
+                minV = 50; //200
+                maxV = 255;
+
+                // inRange(原图像, 最小值的范围, 最大值的范围, 输出图像); //输出图像是黑白二值图像，其中 最小值<=像素点<=最大值 的像素点是白色
+                inRange(hsvImg, Scalar(minH, minS, minV), Scalar(maxH, maxS, maxV), yellowImg); //Threshold the image
+                imshow("yellow", yellowImg);
+                break;
+
+            case 1: //蓝色
+                 minH = 70; //26
+                maxH = 120; //34
+
+                minS = 150; //
+                maxS = 255;
+
+                minV = 50; //200
+                maxV = 255;
+
+                inRange(hsvImg, Scalar(minH, minS, minV), Scalar(maxH, maxS, maxV), blueImg); //Threshold the image
+                imshow("blue", blueImg);
+                break;
+
+            case 2: //红色
+                minH = 0; //26
+                maxH = 10; //34
+
+                minS = 100; //
+                maxS = 255;
+
+                minV = 46; //200
+                maxV = 255;
+
+                inRange(hsvImg, Scalar(minH, minS, minV), Scalar(maxH, maxS, maxV), redImg); 
+                imshow("red", redImg);
+                break;
+
+            case 3: //绿色
+                minH = 35; //26
+                maxH = 77; //34
+
+                minS = 43; //
+                maxS = 255;
+
+                minV = 46; //200
+                maxV = 255;
+
+                inRange(hsvImg, Scalar(minH, minS, minV), Scalar(maxH, maxS, maxV), greenImg); 
+                imshow("green", greenImg);
+                break;
+
+            default:
+                cout << "颜色标志输入错误" << endl;
+        }
+    
+    }
+    // Mat resultImg;
+    // Mat yeBluImg;
+
+    // //bitwise_or(a, b, ab);
+    // //图像或 ，ab = a||b
+
+    // bitwise_or(yellowImg, blueImg, yeBluImg); //图像或 
+    // bitwise_or(yeBluImg, redImg, resultImg); //图像或
+
+    // imshow("result", resultImg);
+
+
+    // //开操作 (去除一些噪点)  如果二值化后图片干扰部分依然很多，增大下面的size
+    Mat eleOpen = getStructuringElement(MORPH_RECT, Size(4, 4));
+    morphologyEx(yellowImg, yellowImg, MORPH_OPEN, eleOpen);
+    morphologyEx(blueImg, blueImg, MORPH_OPEN, eleOpen);
+    morphologyEx(redImg, redImg, MORPH_OPEN, eleOpen);
+    morphologyEx(greenImg, greenImg, MORPH_OPEN, eleOpen);
+    imshow("yellow", yellowImg);
+    imshow("blue", blueImg);
+    imshow("red", redImg);
+    imshow("green", greenImg);
+    // imshow("open", resultImg);
+}
+
+int main()
+{
+    VideoCapture capture(0);
+    Mat image, red_image;
+    while(waitKey(33) != 27)
+    {
+        capture.read(image); //摄像头一帧内容存入mat
+        if(image.empty())
+            break;
+        hsv_detector(image);
+    }
+    return 0;
+}
---
+++ rgb_detector/red_rgb_detector.cpp
@@ -0,0 +1,94 @@
+#include <opencv2/opencv.hpp>
+#include <vector>
+
+using namespace cv;
+using namespace std;
+
+void RTOG(Mat r, int row, int col)
+{
+    int rows = row + 10;
+    int cols = col + 5;
+    for(; row < rows; row++)
+    {
+        for(; col < cols; col++)
+        {
+            r.at<Vec3b>(row, col)[0] = 0;
+            r.at<Vec3b>(row, col)[1] = 255;
+            r.at<Vec3b>(row, col)[2] = 0;
+        }
+    }
+}
+
+void detector(Mat red)
+{
+    int min_row = 1000, min_col = 1000, max_row = 0, max_col = 0;
+
+    Mat r;
+
+    r = red.clone();
+
+    for(int row = 0; row < r.rows; row++)
+    {
+        for(int col = 0; col < r.cols; col++)
+        {
+            if(r.at<Vec3b>(row, col)[2] < 160)
+            {
+                r.at<Vec3b>(row, col)[0] = 0;
+                r.at<Vec3b>(row, col)[1] = 0;
+                r.at<Vec3b>(row, col)[2] = 0;
+            }
+            if(r.at<Vec3b>(row, col)[2] >= 160 && r.at<Vec3b>(row, col)[0] < 100 && r.at<Vec3b>(row, col)[1] < 100)
+            {
+                r.at<Vec3b>(row, col)[0] = 0;
+                r.at<Vec3b>(row, col)[1] = 0;
+            }
+            if(r.at<Vec3b>(row, col)[2] >= 160 && (r.at<Vec3b>(row, col)[0] >= 100 || r.at<Vec3b>(row, col)[1] >= 100))
+            {
+                r.at<Vec3b>(row, col)[0] = 0;
+                r.at<Vec3b>(row, col)[1] = 0;
+                r.at<Vec3b>(row, col)[2] = 0;
+            }
+        }
+    }
+
+    imshow("image", red);
+    // imshow("detector", r);
+    // printf("width: %d  length: %d", r.rows, r.cols);
+    for(int row = 0; row < r.rows; row++)
+    {
+        for(int col = 0; col < r.cols; col++)
+        {
+            if(r.at<Vec3b>(row, col)[2])
+            {
+                if(row < min_row)
+                    min_row = row;
+                if(row > max_row)
+                    max_row = row;
+                if(col < min_col)
+                    min_col = col;
+                if(col > max_col)
+                    max_col = col;
+            }
+        }
+    }
+    RTOG(r, (min_row + max_row)/2, (min_col + max_col)/2);
+    // r.at<Vec3b>((min_row + max_row)/2, (min_col + max_col)/2)[1] = 255;
+
+    // printf("rows:%d cols:%d\n",( min_row + max_row)/2, (min_col + max_col)/2);
+    imshow("detector", r);
+    waitKey(10);
+}
+
+int main()
+{
+    VideoCapture capture(0);
+    Mat image, red_image;
+    while(waitKey(33) != 27)
+    {
+        capture.read(image); //摄像头一帧内容存入mat
+        if(image.empty())
+            break;
+        detector(image);
+    }
+    return 0;
+}
